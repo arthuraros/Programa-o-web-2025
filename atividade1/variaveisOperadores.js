467fc1a7cbb39146c5da9fe65bf568314e4b89b8
Arthur Aros <133790364+arthuraros@users.noreply.github.com>
2025-03-07 09:06:11 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,11 @@
+// Declaração das variáveis numéricas
+let num_A = 2.47;
+let num_B = 3.14;
+let num_C;
+
+// Operações
+num_C = num_A + num_B;
+
+
+// Impressão dos resultados
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${num_C}`);
